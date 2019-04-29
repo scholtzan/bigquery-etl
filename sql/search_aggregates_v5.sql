d39e8a5bbd14c4ad96b5b5c8cef8aa78543869e7
Allen Short <ashort@mozilla.com>
2019-04-29 12:32:09 -0500

    search aggregates

---
+++ sql/search_aggregates_v5.sql
@@ -0,0 +1,91 @@
+WITH
+  augmented AS (
+  SELECT
+    s.*,
+    sc.element.source AS source,
+    sc.element.count AS count,
+    sc.element.engine AS engine,
+    (
+    SELECT
+      ANY_VALUE(element.version)
+    FROM
+      UNNEST(active_addons.list)
+    WHERE
+      element.addon_id = 'followonsearch@mozilla.com') AS addon_version,
+    CASE
+      WHEN (sc.element.source IN ('searchbar', 'urlbar', 'abouthome', 'newtab', 'contextmenu', 'system', 'activitystream', 'webextension', 'alias') OR sc.element.source IS NULL) THEN 'sap'
+      WHEN STARTS_WITH(sc.element.source, 'in-content:sap:')
+    OR STARTS_WITH(sc.element.source, 'sap:') THEN 'tagged-sap'
+      WHEN STARTS_WITH(sc.element.source, 'in-content:sap-follow-on:') OR STARTS_WITH(sc.element.source,'follow-on:') THEN 'tagged-follow-on'
+      WHEN STARTS_WITH(sc.element.source, 'in-content:organic:') THEN 'organic'
+      ELSE 'unknown'
+    END AS type
+  FROM
+    main_summary_v4 AS s,
+    UNNEST(search_counts.list) AS sc
+  WHERE
+    submission_date_s3 = @submission_date
+    ),
+  aggregated AS (
+  SELECT
+    submission_date_s3 AS submission_date,
+    addon_version,
+    HLL_COUNT.INIT(client_id,
+      12) AS _hll,
+    app_version,
+    country,
+    distribution_id,
+    locale,
+    search_cohort,
+    engine,
+    source,
+    SUM(count) AS count,
+    default_search_engine,
+    type
+  FROM
+    augmented
+  WHERE
+    count < 10000
+    AND engine IS NOT NULL
+  GROUP BY
+    submission_date_s3,
+    addon_version,
+    app_version,
+    country,
+    distribution_id,
+    engine,
+    locale,
+    search_cohort,
+    source,
+    default_search_engine,
+    type)
+SELECT
+  submission_date,
+  addon_version,
+  app_version,
+  country,
+  distribution_id,
+  engine,
+  locale,
+  search_cohort,
+  source,
+  default_search_engine,
+  HLL_COUNT.MERGE(_hll) AS client_count,
+  SUM(IF(type = 'organic', count, 0)) AS organic,
+  SUM(IF(type = 'tagged-sap', count, 0)) AS tagged_sap,
+  SUM(IF(type = 'tagged-follow-on', count, 0)) AS tagged_follow_on,
+  SUM(IF(type = 'sap', count, 0)) AS sap,
+  SUM(IF(type = 'unknown', count, 0)) AS unknown
+FROM
+  aggregated
+GROUP BY
+  submission_date,
+  addon_version,
+  app_version,
+  country,
+  distribution_id,
+  locale,
+  search_cohort,
+  engine,
+  source,
+  default_search_engine
